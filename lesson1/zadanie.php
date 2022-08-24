be14deeec93f4f1f1321b86b18d987543130ff94
Sergey <uporovo@mail.ru>
2022-08-24 16:46:13 +0500

    rab

--- lesson1/zadanie.php
+++ lesson1/zadanie.php
@@ -1,10 +1,32 @@
 <?php
+//ini_set('error_reporting', E_ALL);
+//ini_set('display_errors', 1);
+//ini_set('display_startup_errors', 1);
 class Tag
 {
+    public $md = [];
     public $attr = [];
+
     public function attr($attr, $zn)
     {
-        $this->attr[] = array($attr, $zn);
+        $this->attr[$attr] = $zn;
+    }
+
+    public function render()
+    {
+        $res = '<' . $this->tag . ' ';
+
+            foreach ($this->attr as $key => $value) {
+                $res .= $key . '="' . $value . '" ';
+            }
+        $res .= '>';
+        foreach ($this->PairAttr as $value)
+        {
+            $res .= $value . ' ';
+        }
+        $res .= '</'.$this->tag.'>';
+
+        return $res;
     }
 }
 
@@ -22,23 +44,42 @@ class SingleTag extends Tag
 
 class PairTag extends Tag
 {
+    public $test = '';
     public $tag = '';
-    public $test;
+    public $PairAttr = [];
+
     public function __construct(string $tag)
     {
         $this->tag=$tag;
+
     }
     public function appendChild(Tag $attr)
     {
-        echo "<pre>";
-       // echo "<pre>";
-      //  print_r($attr);
-        foreach ($attr as list ($a, $b, $c)) {
-print_r($c);
+        if ($attr instanceof SingleTag)
+        {
+            $this->test  = "<".$attr->Stag . " ";
+            $this->test .= '';
+            foreach ($attr as $value) {
+                foreach ($value as $item=>$key){
+                    $this->test .= $item . '="' . $key . '" ';
+                }
+            }
+            $this->test .= ">";
+            $this->PairAttr[] = $this->test;
+            //return $this->test;
+            $this->test = '';
 
-
+            return $this->PairAttr;
         }
-    }
+        elseif($attr instanceof PairTag){
+
+            $this->md [] = $this->tag;
+            return $this->md;
+        }
+        }
+
+
+
 }
 
 $img = new SingleTag('img');
@@ -46,7 +87,27 @@ $img->attr('src', './nz');
 $img->attr('alt', 'nz');
 $img->attr('width', '100');
 
+
+$hr = new SingleTag('hr');
+
+
+
 $a = new PairTag('a');
+$a->attr('href', './nz');
 $a->appendChild($img);
+$a->appendChild($hr);
+
+
+$a->render();
+
 //echo "<pre>";
-//print_r($img);
+//print_r($b);
+$ec = htmlspecialchars ($a->render());
+echo $ec;
+//echo '<br>';
+//$b->appendChild($a);
+
+//echo htmlspecialchars($a->appendChild($img));
+//echo '<br>';
+//echo "<pre>";
+//htmlspecialchars(print_r($b));
